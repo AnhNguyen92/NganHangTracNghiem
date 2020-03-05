1033e5bed697a6ee94a7a0d18200f199665932a0
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-03-05 18:35:36 +0700

    init project

---
+++ src/main/java/vn/com/ids/multiplechoice/MultipleChoiceApplication.java
@@ -0,0 +1,13 @@
+package vn.com.ids.multiplechoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleChoiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/vn/com/ids/multiplechoice/web/config/WebConfig.java
@@ -0,0 +1,24 @@
+package vn.com.ids.multiplechoice.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer  {
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/imgs/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+}
