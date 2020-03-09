65844d1623dcd5ba1bcf1d7eb0865dc9ee111061
TuanAnh <anh.nguyen@idsolutions.com.vn>
2020-03-09 09:43:12 +0700

    create question
    add code for create question

--- src/main/java/vn/com/ids/multiplechoice/web/controller/MultipleChoiceQuestionController.java
+++ src/main/java/vn/com/ids/multiplechoice/web/controller/MultipleChoiceQuestionController.java
@@ -2,21 +2,26 @@ package vn.com.ids.multiplechoice.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import vn.com.ids.multiplechoice.web.model.MultipleChoiceQuestion;
 
 @Controller
 @RequestMapping(value = "/question")
 public class MultipleChoiceQuestionController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String createQuestion() {
+    @GetMapping("/")
+    public String createQuestion(Model model) {
+    	model.addAttribute("question", new MultipleChoiceQuestion());
+    	
         return "create-question";
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String f(Model model, @RequestParam("") String question, @RequestParam("") String anser1, @RequestParam("") String anser2, @RequestParam("") String anser3, @RequestParam("") String anser4,
+    @PostMapping
+    public String saveQuestion(Model model, @RequestParam("") String question, @RequestParam("") String anser1, @RequestParam("") String anser2, @RequestParam("") String anser3, @RequestParam("") String anser4,
             @RequestParam("") String trueanswer1) {
         String target = "index";
         boolean isvalidInfo = validFormData();
--- src/main/java/vn/com/ids/multiplechoice/web/model/MultipleChoiceQuestion.java
+++ src/main/java/vn/com/ids/multiplechoice/web/model/MultipleChoiceQuestion.java
@@ -1,11 +1,45 @@
 package vn.com.ids.multiplechoice.web.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MultipleChoiceQuestion {
-    private String question;
-    private String anser1;
-    private String anser2;
-    private String anser3;
-    private String anser4;
     
+    private String question;
+    // list answer of question
+    private List<String> answer;
+    // list checkbox to know which one answer is corect.
+    private List<String> checkboxLst;
+    
+	public String getQuestion() {
+		return question;
+	}
+	
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	
+	public List<String> getAnswer() {
+		return answer;
+	}
+	
+	public void setAnswer(List<String> answer) {
+		this.answer = answer;
+	}
+
+    public List<String> getCheckbox() {
+        return checkboxLst;
+    }
+
+    public void setCheckbox(List<String> checkboxLst) {
+        this.checkboxLst = checkboxLst;
+    }
+
+    public MultipleChoiceQuestion() {
+        this.question = "";
+        this.answer  = new ArrayList<String>();
+        this.checkboxLst = Arrays.asList("A", "B", "C", "D");
+    }
     
 }
