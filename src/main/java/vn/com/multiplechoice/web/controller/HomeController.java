f3aa4a41d98d6e1078afbd94888e54dff43cf6cb
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-06-04 14:46:07 +0700

    rename class

--- src/main/java/vn/com/multiplechoice/web/controller/HomeController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/HomeController.java
@@ -3,15 +3,14 @@ package vn.com.multiplechoice.web.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class HomeController {
 	@Value("${welcome.message}")
 	private String message;
 
-	@RequestMapping(value = {"/", "index"}, method = RequestMethod.GET)
+	@GetMapping(value = {"/", "index"})
 	public String index(Model model) {
 		model.addAttribute("message", message);
 
