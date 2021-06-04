83c3443b32f0beef291c0c6bc7a3adcd8b97e389
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-06-04 17:32:42 +0700

    update header logo

--- src/main/java/vn/com/multiplechoice/web/config/WebConfig.java
+++ src/main/java/vn/com/multiplechoice/web/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/imgs/**").addResourceLocations("classpath:/static/imgs/");
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-        registry.addResourceHandler("/fontawesome/**").addResourceLocations("classpath:/static/fontawesome/");
+        registry.addResourceHandler("/fontawesome/**").addResourceLocations("classpath:/static/vendor/fontawesome/");
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 
