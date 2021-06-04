b398ca24c56e1d870fa64815e53f95d9a254dc08
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-06-04 17:50:58 +0700

    update rich text editor

---
+++ src/main/java/vn/com/multiplechoice/dao/model/Test.java
@@ -0,0 +1,14 @@
+package vn.com.multiplechoice.dao.model;
+
+public class Test {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
--- src/main/java/vn/com/multiplechoice/web/controller/PrintFileFinalController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/PrintFileFinalController.java
@@ -3,15 +3,28 @@ package vn.com.multiplechoice.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import vn.com.multiplechoice.dao.model.Test;
+
 @Controller
-@RequestMapping("print-file-final")
+@RequestMapping("/print-file-final")
 public class PrintFileFinalController {
 
     @GetMapping
     public String addHeaderAndFooter(Model model) {
+        
+        Test test = new Test();
+        test.setContent("--- Hết ---");
+        model.addAttribute("test", test);
         return "print-file-final";
     }
 
+    @PostMapping
+    public String save(Test test, Model model) {
+        model.addAttribute("test", test);
+        return "saved";
+    }
+    
 }
