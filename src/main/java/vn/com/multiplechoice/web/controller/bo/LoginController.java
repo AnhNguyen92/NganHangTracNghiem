53704ed3d442b39e5386ca0e1ea7ec436a77ab67
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-06-24 09:28:12 +0700

    add auto config bean name for same class name

--- src/main/java/vn/com/multiplechoice/web/controller/bo/LoginController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/bo/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller(value = "boLoginController")
+@Controller
 @RequestMapping("/bo/login")
 public class LoginController {
 
