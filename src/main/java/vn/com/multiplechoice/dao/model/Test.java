8f379db299ee130b1123813888c88b1d8dd72ddd
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-07-17 02:14:31 +0700

    add test model

--- src/main/java/vn/com/multiplechoice/dao/model/Test.java
+++ src/main/java/vn/com/multiplechoice/dao/model/Test.java
@@ -1,8 +1,35 @@
 package vn.com.multiplechoice.dao.model;
 
-public class Test {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "test")
+@Entity
+public class Test implements Serializable {
+
+    private static final long serialVersionUID = -5540956405687315750L;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    
+    @Column(name = "content")
     private String content;
 
+    @Column(name = "num_of_question")
+    private int numOfQuestions;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User creator;
     public String getContent() {
         return content;
     }
