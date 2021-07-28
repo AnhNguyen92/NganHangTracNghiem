d822b41b33f9eb85290c900e32b29cb72bc9f4ac
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-07-29 06:19:21 +0700

    update contact ui

--- src/main/java/vn/com/multiplechoice/web/controller/fo/ContactController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/ContactController.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import vn.com.multiplechoice.business.service.UserRequestService;
 import vn.com.multiplechoice.dao.model.User;
 import vn.com.multiplechoice.dao.model.UserRequest;
 import vn.com.multiplechoice.web.utils.OnlineUserUtil;
@@ -19,23 +22,27 @@ import vn.com.multiplechoice.web.utils.OnlineUserUtil;
 public class ContactController {
     private static final Logger logger = LoggerFactory.getLogger(ContactController.class);
 
+    @Autowired
     private OnlineUserUtil onlineUserUtil;
+    
+    @Autowired
+    private UserRequestService userRequestService;
     
     @GetMapping
     public String contact(Model model) {
         logger.info("START get contact");
+        model.addAttribute("userRequest", new UserRequest());
+        
         return "fo/contact";
     }
     
     @PostMapping
-    public String sendRequest(Model model, String message) {
+    public String sendRequest(Model model, @ModelAttribute("userRequest") UserRequest userRequest) {
         logger.info("START check request");
         User user = onlineUserUtil.getOnlineUser();
-        UserRequest userRequest = new UserRequest();
-        userRequest.setContent(message);
         userRequest.setCreateDate(LocalDate.now());
         userRequest.setUser(user);
-        
+        userRequestService.save(userRequest);
         
         model.addAttribute("message", "Yêu cầu đã được gửi tới admin!");
         
