68de65c2c7a0b8449c8a8d1d5e25a080e9b4dd0c
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-08-01 05:52:02 +0700

    update question type enum

--- src/main/java/vn/com/multiplechoice/dao/model/enums/QuestionType.java
+++ src/main/java/vn/com/multiplechoice/dao/model/enums/QuestionType.java
@@ -1,9 +1,11 @@
 package vn.com.multiplechoice.dao.model.enums;
 
 public enum QuestionType {
-    ONE_ANSWER, //
-    MULTIPLE_ANSWER, //
-    MATCHING, //
-    COMPLETION_1, //
-    COMPLETION_2;
+    ONE_ANSWER,      // 1 CAU TRA LOI DUNG
+    MULTIPLE_ANSWER, // NHIEU CAU TRA LOI DUNG
+    MATCHING,        // GHEP CAP
+    COMPLETION_1,    // DIEN KHUYET LOAI 1
+    COMPLETION_2,    // DIEN KHUYET LOAI 2, DUNG CHO DOAN VAN NHU BAI KIEM TRA TIENG ANH
+    UDERLINE,        // GACH CHAN
+    TRUE_FALSE;      // DUNG SAI
 }
