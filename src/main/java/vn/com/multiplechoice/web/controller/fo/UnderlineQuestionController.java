791a0703abaa8a0fdcc2a5bb04cb2ee35e481fc2
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-08-02 15:50:20 +0700

    update under line question

--- src/main/java/vn/com/multiplechoice/web/controller/fo/UnderlineQuestionController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/UnderlineQuestionController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,8 +30,12 @@ import vn.com.multiplechoice.web.model.QuestionAnswerDto;
 @Controller
 public class UnderlineQuestionController {
 
+    private static final String FO_CREATE_QUESTION_UNDERLINE = "fo/create-question-underline";
     private static final Logger log = LoggerFactory.getLogger(UnderlineQuestionController.class);
-    
+    private static final String[] ANSWER_LABELS = new String[] { "Đáp án A", "Đáp án B", "Đáp án C", "Đáp án D", "Đáp án E", "Đáp án F", "Đáp án G",
+            "Đáp án H" };
+    private static final String MCQ_DTO = "mcqDto";
+
     @Autowired
     private UserService userService;
 
@@ -48,9 +53,46 @@ public class UnderlineQuestionController {
         }
 
         mcqDto.setQuestionAnswerDtos(questionAnswerDtos);
-        model.addAttribute("mcqDto", mcqDto);
+        model.addAttribute(MCQ_DTO, mcqDto);
 
-        return "fo/create-question-true-false-short";
+        return FO_CREATE_QUESTION_UNDERLINE;
+    }
+
+    @RequestMapping(value = "/underline", params = { "add-answer" })
+    public String addAnswer(Model model, final MCQDto mcqDto, final BindingResult result) {
+        List<QuestionAnswerDto> answerDtos = mcqDto.getQuestionAnswerDtos();
+        if (CollectionUtils.isEmpty(mcqDto.getQuestionAnswerDtos())) {
+            answerDtos = new ArrayList<>();
+        }
+        for (int i = 0; i < answerDtos.size(); i++) {
+            QuestionAnswerDto questionAnswerDto = mcqDto.getQuestionAnswerDtos().get(i);
+            questionAnswerDto.setOrder(i);
+            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+        }
+
+        QuestionAnswerDto newQuestionAnswerDto = new QuestionAnswerDto();
+        newQuestionAnswerDto.setAnswerLabel(ANSWER_LABELS[answerDtos.size()]);
+        answerDtos.add(newQuestionAnswerDto);
+        mcqDto.setQuestionAnswerDtos(answerDtos);
+
+        model.addAttribute(MCQ_DTO, mcqDto);
+
+        return FO_CREATE_QUESTION_UNDERLINE;
+    }
+
+    @RequestMapping(value = "/underline", params = { "remove-answer" })
+    public String removeAnswer(Model model, MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
+        List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
+        String index = req.getParameter("remove-answer");
+        questionAnswerDtos.remove(questionAnswerDtos.get(Integer.parseInt(index)));
+        for (int i = 0; i < questionAnswerDtos.size(); i++) {
+            QuestionAnswerDto questionAnswerDto = questionAnswerDtos.get(i);
+            questionAnswerDto.setOrder(i);
+            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+        }
+        model.addAttribute(MCQ_DTO, mcqDto);
+
+        return FO_CREATE_QUESTION_UNDERLINE;
     }
 
     @PostMapping("/underline")
@@ -71,5 +113,5 @@ public class UnderlineQuestionController {
 
         return "fo/index";
     }
-    
+
 }
