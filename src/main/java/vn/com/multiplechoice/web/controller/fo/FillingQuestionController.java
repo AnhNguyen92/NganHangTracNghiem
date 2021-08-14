f0e14ac30e290c8e9e031d2211dc5929c2131b47
TuanAnh <anh.nguyen@idsolutions.com.vn>
2021-08-15 01:15:21 +0700

    update question filling type 1

--- src/main/java/vn/com/multiplechoice/web/controller/fo/FillingQuestionController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/FillingQuestionController.java
@@ -30,7 +30,7 @@ import vn.com.multiplechoice.web.model.QuestionAnswerDto;
 @Controller
 public class FillingQuestionController {
 
-    private static final String FO_CREATE_QUESTION_UNDERLINE = "fo/create-question-underline";
+    private static final String FO_CREATE_QUESTION_FILLING_TYPE_1 = "fo/create-question-flling-type-1";
     private static final Logger log = LoggerFactory.getLogger(FillingQuestionController.class);
     private static final String[] ANSWER_LABELS = new String[] { "Đáp án A", "Đáp án B", "Đáp án C", "Đáp án D", "Đáp án E", "Đáp án F", "Đáp án G",
             "Đáp án H" };
@@ -42,7 +42,7 @@ public class FillingQuestionController {
     @Autowired
     private QuestionService questionService;
 
-    @RequestMapping("/filling")
+    @RequestMapping("/filling-type-1")
     public String createUnderlineQuestion(Model model, MCQDto mcqDto) {
         log.info("===== GET underline question form =====");
 
@@ -55,10 +55,10 @@ public class FillingQuestionController {
         mcqDto.setQuestionAnswerDtos(questionAnswerDtos);
         model.addAttribute(MCQ_DTO, mcqDto);
 
-        return FO_CREATE_QUESTION_UNDERLINE;
+        return FO_CREATE_QUESTION_FILLING_TYPE_1;
     }
 
-    @RequestMapping(value = "/filling", params = { "add-answer" })
+    @RequestMapping(value = "/filling-type-1", params = { "add-answer" })
     public String addAnswer(Model model, final MCQDto mcqDto, final BindingResult result) {
         List<QuestionAnswerDto> answerDtos = mcqDto.getQuestionAnswerDtos();
         if (CollectionUtils.isEmpty(mcqDto.getQuestionAnswerDtos())) {
@@ -77,10 +77,10 @@ public class FillingQuestionController {
 
         model.addAttribute(MCQ_DTO, mcqDto);
 
-        return FO_CREATE_QUESTION_UNDERLINE;
+        return FO_CREATE_QUESTION_FILLING_TYPE_1;
     }
 
-    @RequestMapping(value = "/filling", params = { "remove-answer" })
+    @RequestMapping(value = "/filling-type-1", params = { "remove-answer" })
     public String removeAnswer(Model model, MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
         List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
         String index = req.getParameter("remove-answer");
@@ -92,7 +92,7 @@ public class FillingQuestionController {
         }
         model.addAttribute(MCQ_DTO, mcqDto);
 
-        return FO_CREATE_QUESTION_UNDERLINE;
+        return FO_CREATE_QUESTION_FILLING_TYPE_1;
     }
 
     @PostMapping("/filling")
