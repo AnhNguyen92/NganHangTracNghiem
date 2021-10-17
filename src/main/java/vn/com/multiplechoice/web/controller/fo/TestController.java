b0713536b2573a5e8192359651f2fdc60c9dce5b
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-10-17 17:11:24 +0700

    add convert file docx to html

--- src/main/java/vn/com/multiplechoice/web/controller/fo/TestController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/TestController.java
@@ -1,7 +1,9 @@
 package vn.com.multiplechoice.web.controller.fo;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -12,6 +14,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.zwobble.mammoth.DocumentConverter;
+import org.zwobble.mammoth.Result;
 
 import vn.com.multiplechoice.business.service.QuestionService;
 import vn.com.multiplechoice.business.service.TestService;
@@ -49,7 +55,7 @@ public class TestController {
 	}
 
 	@PostMapping
-	public String save(Options options, Model model) {
+	public String save(Options options,@RequestParam("file") MultipartFile multipartFile, Model model) {
 		model.addAttribute("options", options);
 		Test test = new Test();
 
@@ -59,15 +65,30 @@ public class TestController {
 			Question question = questionService.findOne(Long.parseLong(idStr));
 			questions.add(question);
 		}
-
+		
+		// header doc, docx
+		DocumentConverter converter = new DocumentConverter();
+		try {
+			Result<String> result = converter.convertToHtml(multipartFile.getInputStream());
+			String html = result.getValue(); // The generated HTML
+			logger.info("html:\n {}", html);
+			Set<String> warnings = result.getWarnings();
+			logger.info("warnings: \n{}", warnings);
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+		
+		
 		test.setNumOfQuestions(selecteds.size());
 		User creator = onlineUserUtil.getOnlineUser();
 		test.setCreator(creator);
 		test.setContent(options.getContent());
 		test.setQuestions(questions);
-		testService.save(test);
+		//testService.save(test);
 
 		return "fo/saved";
 	}
 
+
+	
 }
