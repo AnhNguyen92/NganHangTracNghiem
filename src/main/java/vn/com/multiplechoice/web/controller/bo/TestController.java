1c6729767fe5e04f352e8762967a85e92f256f15
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-10-24 18:02:24 +0700

    add crud for bo test

---
+++ src/main/java/vn/com/multiplechoice/web/controller/bo/TestController.java
@@ -0,0 +1,59 @@
+package vn.com.multiplechoice.web.controller.bo;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.com.multiplechoice.business.service.TestService;
+import vn.com.multiplechoice.dao.model.Test;
+
+@Controller
+@RequestMapping("/bo/tests")
+public class TestController {
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+	@Autowired
+	private TestService testService;
+	
+	public String list(Model model) {
+		logger.info("------- Start get test list -------");
+		List<Test> tests = testService.findAll();
+//		List<Test> tests = testService.findAll(pageAble);
+		model.addAttribute("tests", tests);
+		
+		return "bo/tests";
+	}
+
+	@GetMapping("/{id}")
+	public String detail(Model model, @PathVariable long id) {
+		logger.info("------- Start get test by id = {} -------", id);
+		Test test = testService.findOne(id);
+		if (test == null) {
+			return "bo/errors/404";
+		}
+		model.addAttribute("test", test);
+
+		return "bo/test";
+	}
+
+	public String create(Model model) {
+		logger.info("------- Start create test -------");
+		model.addAttribute("test", new Test());
+		return "bo/test";
+	}
+
+	public String save(Test test) {
+		logger.info("------- Start save new test -------");
+		testService.save(test);
+		
+		return "redirect:bo/tests/" + test.getId();
+	}
+	
+}
