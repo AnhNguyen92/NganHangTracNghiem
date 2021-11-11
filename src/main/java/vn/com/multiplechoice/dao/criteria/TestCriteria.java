ce105e0680fa54106e12668e2b0dadf3a48341de
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-11-12 01:58:01 +0700

    add tostring method, remove unused field

--- src/main/java/vn/com/multiplechoice/dao/criteria/TestCriteria.java
+++ src/main/java/vn/com/multiplechoice/dao/criteria/TestCriteria.java
@@ -7,7 +7,6 @@ import vn.com.multiplechoice.dao.model.enums.TestStatus;
 public class TestCriteria {
 	private String searchText;
 	private TestStatus status;
-	private long startPage;
 	private PageSize size;
 	private DateRange dateRange;
 
@@ -27,14 +26,6 @@ public class TestCriteria {
 		this.status = status;
 	}
 
-	public long getStartPage() {
-		return startPage;
-	}
-
-	public void setStartPage(long startPage) {
-		this.startPage = startPage;
-	}
-
 	public PageSize getSize() {
 		return size;
 	}
@@ -49,6 +40,12 @@ public class TestCriteria {
 
 	public void setDateRange(DateRange dateRange) {
 		this.dateRange = dateRange;
+	}
+
+	@Override
+	public String toString() {
+		return "TestCriteria [searchText=" + searchText + ", status=" + status + ", size=" + size + ", dateRange="
+				+ dateRange + "]";
 	}
 
 }
