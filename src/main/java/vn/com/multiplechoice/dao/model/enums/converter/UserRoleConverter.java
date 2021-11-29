ad939d912de4a075beabd4dee174e5ba4f5ae47a
TuanAnh <anh.nguyen@idsolutions.com.vn>
2021-11-30 03:46:37 +0700

    remove vietnamese for user role

--- src/main/java/vn/com/multiplechoice/dao/model/enums/converter/UserRoleConverter.java
+++ src/main/java/vn/com/multiplechoice/dao/model/enums/converter/UserRoleConverter.java
@@ -1,25 +1,25 @@
-package vn.com.multiplechoice.dao.model.enums.converter;
-
-import java.util.stream.Stream;
-
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
-
-import vn.com.multiplechoice.dao.model.enums.UserRole;
-
-@Converter(autoApply = true)
-public class UserRoleConverter implements AttributeConverter<UserRole, String> {
-    @Override
-    public String convertToDatabaseColumn(UserRole role) {
-        return (role == null) ? null : role.getValue();
-    }
-
-    @Override
-    public UserRole convertToEntityAttribute(String value) {
-        if (value == null) {
-            return null;
-        }
-        return Stream.of(UserRole.values()).filter(c -> c.getValue().equals(value)).findFirst().orElseThrow(IllegalArgumentException::new);
-    }
-
-}
+//package vn.com.multiplechoice.dao.model.enums.converter;
+//
+//import java.util.stream.Stream;
+//
+//import javax.persistence.AttributeConverter;
+//import javax.persistence.Converter;
+//
+//import vn.com.multiplechoice.dao.model.enums.UserRole;
+//
+//@Converter(autoApply = true)
+//public class UserRoleConverter implements AttributeConverter<UserRole, String> {
+//    @Override
+//    public String convertToDatabaseColumn(UserRole role) {
+//        return (role == null) ? null : role.getValue();
+//    }
+//
+//    @Override
+//    public UserRole convertToEntityAttribute(String value) {
+//        if (value == null) {
+//            return null;
+//        }
+//        return Stream.of(UserRole.values()).filter(c -> c.getValue().equals(value)).findFirst().orElseThrow(IllegalArgumentException::new);
+//    }
+//
+//}
