117247482d22d96d7c7d8bb7f9476302d12c46dd
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2021-12-14 18:18:33 +0700

    update code

--- src/main/java/vn/com/multiplechoice/web/controller/fo/FillingQuestionController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/FillingQuestionController.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,8 +28,7 @@ public class FillingQuestionController {
 
     private static final String FO_CREATE_FILLING_QUESTION = "fo/create-flling-question";
     private static final Logger log = LoggerFactory.getLogger(FillingQuestionController.class);
-    private static final String[] ANSWER_LABELS = new String[] { "Đáp án A", "Đáp án B", "Đáp án C", "Đáp án D", "Đáp án E", "Đáp án F", "Đáp án G",
-            "Đáp án H" };
+    private static final String[] ANSWER_LABELS = new String[] { "A", "B", "C", "D", "E", "F", "G", "H" };
     private static final String MCQ_DTO = "mcqDto";
 
     @Autowired
@@ -51,35 +49,52 @@ public class FillingQuestionController {
         return FO_CREATE_FILLING_QUESTION;
     }
 
-    @RequestMapping(value = "/filling", params = { "add-answer" })
-    public String addAnswer(Model model, final MCQDto mcqDto, final BindingResult result) {
-        List<QuestionAnswerDto> answerDtos = mcqDto.getQuestionAnswerDtos();
-        if (CollectionUtils.isEmpty(mcqDto.getQuestionAnswerDtos())) {
-            answerDtos = new ArrayList<>();
-        }
-        for (int i = 0; i < answerDtos.size(); i++) {
+    @RequestMapping(value = "/filling", params = { "remove-left-answer" })
+    public String addAnswer(Model model, final MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
+        List<QuestionAnswerDto> leftAnswerDtos = mcqDto.getLeftAnswerDtos();
+        String index = req.getParameter("remove-left-answer");
+        leftAnswerDtos.remove(leftAnswerDtos.get(Integer.parseInt(index)));
+        for (int i = 0; i < leftAnswerDtos.size(); i++) {
             QuestionAnswerDto questionAnswerDto = mcqDto.getQuestionAnswerDtos().get(i);
             questionAnswerDto.setOrder(i);
-            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+            questionAnswerDto.setAnswerLabel((i + 1) + "");
         }
-
-        QuestionAnswerDto newQuestionAnswerDto = new QuestionAnswerDto();
-        newQuestionAnswerDto.setAnswerLabel(ANSWER_LABELS[answerDtos.size()]);
-        answerDtos.add(newQuestionAnswerDto);
-        mcqDto.setQuestionAnswerDtos(answerDtos);
-
+        mcqDto.setQuestionAnswerDtos(leftAnswerDtos);
         model.addAttribute(MCQ_DTO, mcqDto);
 
         return FO_CREATE_FILLING_QUESTION;
     }
 
-    @RequestMapping(value = "/filling", params = { "remove-answer" })
-    public String removeAnswer(Model model, MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
-        List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
-        String index = req.getParameter("remove-answer");
-        questionAnswerDtos.remove(questionAnswerDtos.get(Integer.parseInt(index)));
-        for (int i = 0; i < questionAnswerDtos.size(); i++) {
-            QuestionAnswerDto questionAnswerDto = questionAnswerDtos.get(i);
+    @RequestMapping(value = "/filling", params = { "add-right-answer" })
+    public String addRightAnswer(Model model, MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
+        List<QuestionAnswerDto> rightAnswerDtos = mcqDto.getRightAnswerDtos();
+        if (rightAnswerDtos == null) {
+        	rightAnswerDtos = new ArrayList<>();
+        }
+        for (int i = 0; i < rightAnswerDtos.size(); i++) {
+            QuestionAnswerDto questionAnswerDto = rightAnswerDtos.get(i);
+            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+            questionAnswerDto.setOrder(i);
+            questionAnswerDto.setLeftSide(false);
+        }
+        QuestionAnswerDto newItem = new QuestionAnswerDto();
+        newItem.setAnswerLabel(ANSWER_LABELS[rightAnswerDtos.size()]);
+        newItem.setOrder(rightAnswerDtos.size());
+        newItem.setLeftSide(false);
+        rightAnswerDtos.add(newItem);
+        mcqDto.setRightAnswerDtos(rightAnswerDtos);
+        model.addAttribute(MCQ_DTO, mcqDto);
+
+        return FO_CREATE_FILLING_QUESTION;
+    }
+
+    @RequestMapping(value = "/filling", params = { "remove-right-answer" })
+    public String removeRightAnswer(Model model, MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
+        List<QuestionAnswerDto> rightAnswerDtos = mcqDto.getRightAnswerDtos();
+        String index = req.getParameter("remove-right-answer");
+        rightAnswerDtos.remove(rightAnswerDtos.get(Integer.parseInt(index)));
+        for (int i = 0; i < rightAnswerDtos.size(); i++) {
+            QuestionAnswerDto questionAnswerDto = rightAnswerDtos.get(i);
             questionAnswerDto.setOrder(i);
             questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
         }
@@ -93,7 +108,7 @@ public class FillingQuestionController {
         log.info("===== START create filling question form =====");
         
         Question question = questionConverter.toEntity(mcqDto);
-        questionService.save(question);
+        //questionService.save(question);
 
         log.info("===== CREATE filling question form END =====");
 
@@ -129,6 +144,7 @@ public class FillingQuestionController {
 
         return "fo/create-group-filling-question";
     }
+
     @PostMapping("/group-filling")
     public String saveGroupFillingQuestion(Model model, final @ModelAttribute("mcqDto") MCQDto mcqDto, final BindingResult result) {
         log.info("===== START create group filling question form =====");
@@ -140,4 +156,5 @@ public class FillingQuestionController {
 
         return "redirect:/fo/index";
     }
+
 }
