2b3c913c8f7b85d46c3acf177488ea0ce45d30b6
TuanAnh <anh.nguyen@idsolutions.com.vn>
2022-03-30 05:07:56 +0700

    remove unused import class

--- src/main/java/vn/com/multiplechoice/web/controller/bo/TestController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/bo/TestController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
