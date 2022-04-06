9ca30311c3b889d20716feb830107632cd02baae
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-04-06 14:14:10 +0700

    add default height when website has no data

---
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/CustomErrorController.java
@@ -0,0 +1,38 @@
+package vn.com.multiplechoice.web.controller.fo;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+	@GetMapping("/fo/error")
+	public String handleError(HttpServletRequest request) {
+		String errorPage = "error";
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+		if (status != null) {
+			int statusCode = Integer.parseInt(status.toString());
+
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				errorPage = "fo/404";
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				errorPage = "fo/403";
+			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				errorPage = "fo/500";
+			}
+		}
+
+		return errorPage;
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/fo/error";
+	}
+}
