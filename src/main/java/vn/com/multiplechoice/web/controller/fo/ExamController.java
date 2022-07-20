6a36ec8d4a834e6b22fb326f7e860e6cefb6155a
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-07-20 22:17:53 +0700

    update lastest code

--- src/main/java/vn/com/multiplechoice/web/controller/fo/ExamController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/ExamController.java
@@ -1,5 +1,9 @@
 package vn.com.multiplechoice.web.controller.fo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import vn.com.multiplechoice.business.converter.TestConverter;
 import vn.com.multiplechoice.business.service.TestService;
+import vn.com.multiplechoice.dao.model.Question;
 import vn.com.multiplechoice.dao.model.Test;
 import vn.com.multiplechoice.web.dto.ExamDto;
+import vn.com.multiplechoice.web.model.MCQDto;
 
 @Controller
 @RequestMapping(value = "/fo/do-exam")
@@ -41,8 +47,34 @@ public class ExamController {
 	
 	@PostMapping("")
 	public String doExam(Model model,@ModelAttribute ExamDto examDto) {
-		System.out.println(examDto);
+		log.info("{}", examDto);
+		markExam(examDto);
+		
 		return "redirect:/fo/index";
+	}
+
+	private void markExam(ExamDto examDto) {
+		double totalScore = 0.0;
+		int totalRightAnswer = 0;
+		Test test = testService.findById(examDto.getId());
+		Set<Question> questions = test.getQuestions();
+		double pointPerQuestion = 10.0 / questions.size();
+		for (MCQDto mcqDto : examDto.getQuestions()) {
+			Question question = questions.stream().filter(q-> q.getId().equals(mcqDto.getId())).findFirst().orElseThrow();
+			List<String> rightAnswerLst = Arrays.asList(question.getRightAnswer().split(",")) ;
+			if (rightAnswerLst.equals(mcqDto.getSelectedAnswers())) {
+				log.info("found true question");
+				totalRightAnswer++;
+				if (question.getScore() == null) {
+					totalScore += pointPerQuestion;
+				} else {
+					rightAnswerLst.retainAll(mcqDto.getSelectedAnswers());
+					totalScore +=  pointPerQuestion * rightAnswerLst.size();
+				}
+//				totalScore +=  question.getScore();
+			}
+		}
+		
 	}
 
 }
