0f17926b78b390f5756304a5d28ca09d4c834423
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-07-26 18:11:06 +0700

    Update ExamController.java

--- src/main/java/vn/com/multiplechoice/web/controller/fo/ExamController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/ExamController.java
@@ -25,93 +25,94 @@ import vn.com.multiplechoice.web.model.MCQDto;
 @Controller
 @RequestMapping(value = "/fo/do-exam")
 public class ExamController {
-    private static final Logger log = LoggerFactory.getLogger(ExamController.class);
+	private static final Logger log = LoggerFactory.getLogger(ExamController.class);
 
-    @Autowired
-    TestService testService;
+	@Autowired
+	TestService testService;
 
-    @Autowired
-    TestConverter testConverter;
+	@Autowired
+	TestConverter testConverter;
 
-    @GetMapping("/{id}")
-    public String exam(Model model, @PathVariable("id") Long id) {
-        log.info("===  Start do exam with test has id = {}  ===", id);
-        Test test = testService.findById(id);
-        String executeTime = test.getExecuteTime();
+	@GetMapping("/{id}")
+	public String exam(Model model, @PathVariable("id") Long id) {
+		log.info("===  Start do exam with test has id = {}  ===", id);
+		Test test = testService.findById(id);
+		String executeTime = test.getExecuteTime();
 
-        model.addAttribute("executeTime", executeTime);
-        ExamDto examDto = testConverter.toExam(test);
-        model.addAttribute("examDto", examDto);
-        return "/fo/do-exam";
-    }
+		model.addAttribute("executeTime", executeTime);
+		ExamDto examDto = testConverter.toExam(test);
+		model.addAttribute("examDto", examDto);
+		return "/fo/do-exam";
+	}
 
-    @PostMapping("")
-    public String doExam(Model model, @ModelAttribute ExamDto examDto) {
-        log.info("{}", examDto);
-        markExam(examDto);
+	@PostMapping("")
+	public String doExam(Model model, @ModelAttribute ExamDto examDto) {
+		log.info("{}", examDto);
+		markExam(examDto);
 
-        return "redirect:/fo/index";
-    }
+		return "redirect:/fo/index";
+	}
 
-    private void markExam(ExamDto examDto) {
-        double totalScore = 0.0;
-        int totalRightAnswer = 0;
-        Test test = testService.findById(examDto.getId());
-        Set<Question> questions = test.getQuestions();
-        double pointPerQuestion = 10.0 / questions.size();
-        for (MCQDto mcqDto : examDto.getQuestions()) {
-            Question question = questions.stream().filter(q -> q.getId().equals(mcqDto.getId())).findFirst().get();
+	private void markExam(ExamDto examDto) {
+		double totalScore = 0.0;
+		int totalRightAnswer = 0;
+		Test test = testService.findById(examDto.getId());
+		Set<Question> questions = test.getQuestions();
+		double pointPerQuestion = 10.0 / questions.size();
+		for (MCQDto mcqDto : examDto.getQuestions()) {
+			Question question = questions.stream().filter(q -> q.getId().equals(mcqDto.getId())).findFirst().get();
+			List<String> rightAnswerLst = getRightAnswerLst(question);
+			List<String> selectedAnswerLst = mcqDto.getSelectedAnswers();
+			if (rightAnswerLst.equals(selectedAnswerLst)) {
+				log.info("found true question");
+				totalRightAnswer++;
+				if (question.getScore() == null) {
+					totalScore += pointPerQuestion;
+				} else {
+					rightAnswerLst.retainAll(mcqDto.getSelectedAnswers());
+					totalScore += pointPerQuestion * rightAnswerLst.size();
+				}
+//				totalScore +=  question.getScore();
+			}
+		}
+		System.out.println(totalRightAnswer);
+		System.out.println(totalScore);
+		// save to database here
+	}
 
-            List<String> rightAnswerLst = getRightAnswerLst(question);
-            List<String> selectedAnswerLst = mcqDto.getSelectedAnswers();
-            if (rightAnswerLst.equals(selectedAnswerLst)) {
-                log.info("found true question");
-                totalRightAnswer++;
-                if (question.getScore() == null) {
-                    totalScore += pointPerQuestion;
-                } else {
-                    rightAnswerLst.retainAll(mcqDto.getSelectedAnswers());
-                    totalScore += pointPerQuestion * rightAnswerLst.size();
-                }
-                // totalScore += question.getScore();
-            }
-        }
-        // save to database here
-    }
+	private List<String> getRightAnswerLst(Question question) {
+		List<String> rightAnswerLst = new ArrayList<>();
+		String[] labels = question.getRightAnswer().split(",");
+		for (String label : labels) {
+			switch (label) {
+			case "A":
+				rightAnswerLst.add(question.getAnswerA());
+				break;
+			case "B":
+				rightAnswerLst.add(question.getAnswerB());
+				break;
+			case "C":
+				rightAnswerLst.add(question.getAnswerC());
+				break;
+			case "D":
+				rightAnswerLst.add(question.getAnswerD());
+				break;
+			case "E":
+				rightAnswerLst.add(question.getAnswerE());
+				break;
+			case "F":
+				rightAnswerLst.add(question.getAnswerF());
+				break;
+			case "G":
+				rightAnswerLst.add(question.getAnswerG());
+				break;
+			default:
+				rightAnswerLst.add(question.getAnswerH());
+				break;
+			}
 
-    private List<String> getRightAnswerLst(Question question) {
-        List<String> rightAnswerLst = new ArrayList<>();
-        String[] labels = question.getRightAnswer().split(",");
-        for (String label : labels) {
-            switch (label) {
-            case "A":
-                rightAnswerLst.add(question.getAnswerA());
-                break;
-            case "B":
-                rightAnswerLst.add(question.getAnswerB());
-                break;
-            case "C":
-                rightAnswerLst.add(question.getAnswerC());
-                break;
-            case "D":
-                rightAnswerLst.add(question.getAnswerD());
-                break;
-            case "E":
-                rightAnswerLst.add(question.getAnswerE());
-                break;
-            case "F":
-                rightAnswerLst.add(question.getAnswerF());
-                break;
-            case "G":
-                rightAnswerLst.add(question.getAnswerG());
-                break;
-            default:
-                rightAnswerLst.add(question.getAnswerH());
-                break;
-            }
-
-        }
-        return rightAnswerLst;
-    }
+		}
+		return rightAnswerLst;
+	}
 
 }
