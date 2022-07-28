bbfb4427b000623bd271b4e04887f8560bc67364
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-07-29 01:19:19 +0700

    format code

--- src/main/java/vn/com/multiplechoice/web/controller/fo/MCQController.java
+++ src/main/java/vn/com/multiplechoice/web/controller/fo/MCQController.java
@@ -30,164 +30,157 @@ public class MCQController {
 	private static final Logger log = LoggerFactory.getLogger(MCQController.class);
 
 	private static final String REMOVE_ANSWER = "remove-answer";
-    private static final String REDIRECT_FO_INDEX = "redirect:/fo/index";
-    private static final String MCQ_DTO = "mcqDto";
-    private static final String FO_CREATE_ONE_ANS_QUESTION = "fo/create-one-ans-question";
-    private static final String FO_CREATE_MULTIPLE_ANS_QUESTION = "fo/create-multiple-ans-question";
-    private static final String[] ANSWER_LABELS = new String[] { "A", "B", "C", "D", "E", "F", "G", "H" };
-    
-    @Autowired
-    private QuestionService questionService;
-    
-    @Autowired
-    private QuestionConverter questionConverter;
-    
-    @RequestMapping("/one-ans")
-    public String createOneAnswerQuestion(Model model, MCQDto mcqDto) {
-        log.info("===== GET one answer question form =====");
-        mcqDto.setType(QuestionType.ONE_ANSWER);
-        List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
-        if (questionAnswerDtos == null) {
-            questionAnswerDtos = new ArrayList<>();
-        }
-        for (int i = 0; i < 4; i++) {
-            QuestionAnswerDto questionAnswerDto = new QuestionAnswerDto();
-            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
-            questionAnswerDto.setOrder(i);
-            questionAnswerDtos.add(questionAnswerDto);
-        }
-        mcqDto.setQuestionAnswerDtos(questionAnswerDtos);
-        model.addAttribute(MCQ_DTO, mcqDto);
+	private static final String REDIRECT_FO_USER_QUESTION = "redirect:/fo/user/questions";
+	private static final String MCQ_DTO = "mcqDto";
+	private static final String FO_CREATE_ONE_ANS_QUESTION = "fo/create-one-ans-question";
+	private static final String FO_CREATE_MULTIPLE_ANS_QUESTION = "fo/create-multiple-ans-question";
+	private static final String[] ANSWER_LABELS = new String[] { "A", "B", "C", "D", "E", "F", "G", "H" };
 
-        return FO_CREATE_ONE_ANS_QUESTION;
-    }
+	@Autowired
+	private QuestionService questionService;
 
-    @RequestMapping(value = "/one-ans", params = { "add-answer" })
-    public String addAnswer(Model model, final MCQDto mcqDto, final BindingResult result) {
-        for (int i = 0; i < mcqDto.getQuestionAnswerDtos().size(); i++) {
-            QuestionAnswerDto questionAnswerDto = mcqDto.getQuestionAnswerDtos().get(i);
-            questionAnswerDto.setOrder(i);
-            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
-        }
-        QuestionAnswerDto newQuestionAnswerDto = new QuestionAnswerDto();
-        newQuestionAnswerDto.setAnswerLabel(ANSWER_LABELS[mcqDto.getQuestionAnswerDtos().size()]);
-        mcqDto.getQuestionAnswerDtos().add(newQuestionAnswerDto);
-        model.addAttribute(MCQ_DTO, mcqDto);
+	@Autowired
+	private QuestionConverter questionConverter;
 
-        return FO_CREATE_ONE_ANS_QUESTION;
-    }
+	@RequestMapping("/one-ans")
+	public String createOneAnswerQuestion(Model model, MCQDto mcqDto) {
+		log.info("===== GET one answer question form =====");
+		mcqDto.setType(QuestionType.ONE_ANSWER);
+		List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
+		if (questionAnswerDtos == null) {
+			questionAnswerDtos = new ArrayList<>();
+		}
+		for (int i = 0; i < 4; i++) {
+			QuestionAnswerDto questionAnswerDto = new QuestionAnswerDto();
+			questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+			questionAnswerDto.setOrder(i);
+			questionAnswerDtos.add(questionAnswerDto);
+		}
+		mcqDto.setQuestionAnswerDtos(questionAnswerDtos);
+		model.addAttribute(MCQ_DTO, mcqDto);
 
-    @RequestMapping(value = "/one-ans", params = { REMOVE_ANSWER })
-    public String removeAnswer(Model model, MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
-        List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
-        String index = req.getParameter(REMOVE_ANSWER);
-        questionAnswerDtos.remove(questionAnswerDtos.get(Integer.parseInt(index)));
-        for (int i = 0; i < questionAnswerDtos.size(); i++) {
-            QuestionAnswerDto questionAnswerDto = questionAnswerDtos.get(i);
-            questionAnswerDto.setOrder(i);
-            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
-        }
-        model.addAttribute(MCQ_DTO, mcqDto);
+		return FO_CREATE_ONE_ANS_QUESTION;
+	}
 
-        return FO_CREATE_ONE_ANS_QUESTION;
-    }
+	@RequestMapping(value = "/one-ans", params = { "add-answer" })
+	public String addAnswer(Model model, final MCQDto mcqDto, final BindingResult result) {
+		for (int i = 0; i < mcqDto.getQuestionAnswerDtos().size(); i++) {
+			QuestionAnswerDto questionAnswerDto = mcqDto.getQuestionAnswerDtos().get(i);
+			questionAnswerDto.setOrder(i);
+			questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+		}
+		QuestionAnswerDto newQuestionAnswerDto = new QuestionAnswerDto();
+		newQuestionAnswerDto.setAnswerLabel(ANSWER_LABELS[mcqDto.getQuestionAnswerDtos().size()]);
+		mcqDto.getQuestionAnswerDtos().add(newQuestionAnswerDto);
+		model.addAttribute(MCQ_DTO, mcqDto);
 
-    @PostMapping("/one-ans")
-    public String saveOneAnswerQuestion(Model model, final @ModelAttribute(MCQ_DTO) MCQDto mcqDto, final BindingResult result) {
-        log.info("===== START create one answer question form =====");
-        Question question = questionConverter.toEntity(mcqDto);
+		return FO_CREATE_ONE_ANS_QUESTION;
+	}
 
-        questionService.save(question);
+	@RequestMapping(value = "/one-ans", params = { REMOVE_ANSWER })
+	public String removeAnswer(Model model, MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
+		List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
+		String index = req.getParameter(REMOVE_ANSWER);
+		questionAnswerDtos.remove(questionAnswerDtos.get(Integer.parseInt(index)));
+		for (int i = 0; i < questionAnswerDtos.size(); i++) {
+			QuestionAnswerDto questionAnswerDto = questionAnswerDtos.get(i);
+			questionAnswerDto.setOrder(i);
+			questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+		}
+		model.addAttribute(MCQ_DTO, mcqDto);
 
-        log.info("===== CREATE one answer question form END =====");
+		return FO_CREATE_ONE_ANS_QUESTION;
+	}
 
-        return REDIRECT_FO_INDEX;
-    }
+	@PostMapping("/one-ans")
+	public String saveOneAnswerQuestion(Model model, final @ModelAttribute(MCQ_DTO) MCQDto mcqDto,
+			final BindingResult result) {
+		log.info("===== START create one answer question form =====");
+		Question question = questionConverter.toEntity(mcqDto);
 
-    @GetMapping("/one-ans/{id}")
-    public String oneAnswerDetail(Model model, @PathVariable long id) {
-    	Question question = questionService.findById(id);
-    	MCQDto mcqDto = questionConverter.toDto(question);
-    	model.addAttribute(MCQ_DTO, mcqDto);
-    	
-    	return FO_CREATE_ONE_ANS_QUESTION;
-    }
-    
-    @GetMapping("/fragment/{id}")
-    public String questionFragmentDetail(Model model, @PathVariable long id) {
-    	Question question = questionService.findById(id);
-    	MCQDto mcqDto = questionConverter.toDto(question);
-    	model.addAttribute("question", mcqDto);
-    	
-    	return "fragments/fo/question";
-    }
-    
-    @RequestMapping("/multiple-ans")
-    public String createMultipleAnswerQuestion(Model model, MCQDto mcqDto) {
-        log.info("===== GET multiple answer question form =====");
-        mcqDto.setType(QuestionType.MULTIPLE_ANSWER);
-        List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
-        if (questionAnswerDtos == null) {
-            questionAnswerDtos = new ArrayList<>();
-        }
-        for (int i = 0; i < 4; i++) {
-            QuestionAnswerDto questionAnswerDto = new QuestionAnswerDto();
-            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
-            questionAnswerDto.setOrder(i);
-            questionAnswerDto.setScore(0);
-            questionAnswerDtos.add(questionAnswerDto);
-        }
-        mcqDto.setQuestionAnswerDtos(questionAnswerDtos);
-        model.addAttribute(MCQ_DTO, mcqDto);
+		questionService.save(question);
 
-        return FO_CREATE_MULTIPLE_ANS_QUESTION;
-    }
+		log.info("===== CREATE one answer question form END =====");
 
-    @RequestMapping(value = "/multiple-ans", params = { "add-answer" })
-    public String addAnswerMultipleQuestion(Model model, final MCQDto mcqDto, final BindingResult result) {
-    	log.info("===== ADD answer for multiple answer question form =====");
-        for (int i = 0; i < mcqDto.getQuestionAnswerDtos().size(); i++) {
-            QuestionAnswerDto questionAnswerDto = mcqDto.getQuestionAnswerDtos().get(i);
-            questionAnswerDto.setOrder(i);
-            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
-        }
-        QuestionAnswerDto newQuestionAnswerDto = new QuestionAnswerDto();
-        newQuestionAnswerDto.setAnswerLabel(ANSWER_LABELS[mcqDto.getQuestionAnswerDtos().size()]);
-        newQuestionAnswerDto.setOrder(mcqDto.getQuestionAnswerDtos().size() - 1);
-        mcqDto.getQuestionAnswerDtos().add(newQuestionAnswerDto);
-        
-        model.addAttribute(MCQ_DTO, mcqDto);
+		return REDIRECT_FO_USER_QUESTION;
+	}
 
-        return FO_CREATE_MULTIPLE_ANS_QUESTION;
-    }
+	@GetMapping("/fragment/{id}")
+	public String questionFragmentDetail(Model model, @PathVariable long id) {
+		Question question = questionService.findById(id);
+		MCQDto mcqDto = questionConverter.toDto(question);
+		model.addAttribute("question", mcqDto);
 
-    @RequestMapping(value = "/multiple-ans", params = { REMOVE_ANSWER })
-    public String removeAnswerMultipleQuestion(Model model, MCQDto mcqDto, final BindingResult result, final HttpServletRequest req) {
-    	log.info("===== REMOVE answer for multiple answer question form =====");
-    	List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
-        String index = req.getParameter(REMOVE_ANSWER);
-        questionAnswerDtos.remove(questionAnswerDtos.get(Integer.parseInt(index)));
-        for (int i = 0; i < questionAnswerDtos.size(); i++) {
-            QuestionAnswerDto questionAnswerDto = questionAnswerDtos.get(i);
-            questionAnswerDto.setOrder(i);
-            questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
-        }
-        model.addAttribute(MCQ_DTO, mcqDto);
+		return "fragments/fo/question";
+	}
 
-        return FO_CREATE_MULTIPLE_ANS_QUESTION;
-    }
+	@RequestMapping("/multiple-ans")
+	public String createMultipleAnswerQuestion(Model model, MCQDto mcqDto) {
+		log.info("===== GET multiple answer question form =====");
+		mcqDto.setType(QuestionType.MULTIPLE_ANSWER);
+		List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
+		if (questionAnswerDtos == null) {
+			questionAnswerDtos = new ArrayList<>();
+		}
+		for (int i = 0; i < 4; i++) {
+			QuestionAnswerDto questionAnswerDto = new QuestionAnswerDto();
+			questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+			questionAnswerDto.setOrder(i);
+			questionAnswerDto.setScore(0);
+			questionAnswerDtos.add(questionAnswerDto);
+		}
+		mcqDto.setQuestionAnswerDtos(questionAnswerDtos);
+		model.addAttribute(MCQ_DTO, mcqDto);
 
-    @PostMapping("/multiple-ans")
-    public String saveMultipleAnswerQuestion(Model model, final @ModelAttribute(MCQ_DTO) MCQDto mcqDto, final BindingResult result) {
-        log.info("===== START create multiple answer question form =====");
-        Question question = questionConverter.toEntity(mcqDto);
-        
-        questionService.save(question);
-       log.info("{}",question);
-        log.info("===== CREATE multiple answer question form END =====");
+		return FO_CREATE_MULTIPLE_ANS_QUESTION;
+	}
 
-        return REDIRECT_FO_INDEX;
-    }
+	@RequestMapping(value = "/multiple-ans", params = { "add-answer" })
+	public String addAnswerMultipleQuestion(Model model, final MCQDto mcqDto, final BindingResult result) {
+		log.info("===== ADD answer for multiple answer question form =====");
+		for (int i = 0; i < mcqDto.getQuestionAnswerDtos().size(); i++) {
+			QuestionAnswerDto questionAnswerDto = mcqDto.getQuestionAnswerDtos().get(i);
+			questionAnswerDto.setOrder(i);
+			questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+		}
+		QuestionAnswerDto newQuestionAnswerDto = new QuestionAnswerDto();
+		newQuestionAnswerDto.setAnswerLabel(ANSWER_LABELS[mcqDto.getQuestionAnswerDtos().size()]);
+		newQuestionAnswerDto.setOrder(mcqDto.getQuestionAnswerDtos().size() - 1);
+		mcqDto.getQuestionAnswerDtos().add(newQuestionAnswerDto);
 
-    
+		model.addAttribute(MCQ_DTO, mcqDto);
+
+		return FO_CREATE_MULTIPLE_ANS_QUESTION;
+	}
+
+	@RequestMapping(value = "/multiple-ans", params = { REMOVE_ANSWER })
+	public String removeAnswerMultipleQuestion(Model model, MCQDto mcqDto, final BindingResult result,
+			final HttpServletRequest req) {
+		log.info("===== REMOVE answer for multiple answer question form =====");
+		List<QuestionAnswerDto> questionAnswerDtos = mcqDto.getQuestionAnswerDtos();
+		String index = req.getParameter(REMOVE_ANSWER);
+		questionAnswerDtos.remove(questionAnswerDtos.get(Integer.parseInt(index)));
+		for (int i = 0; i < questionAnswerDtos.size(); i++) {
+			QuestionAnswerDto questionAnswerDto = questionAnswerDtos.get(i);
+			questionAnswerDto.setOrder(i);
+			questionAnswerDto.setAnswerLabel(ANSWER_LABELS[i]);
+		}
+		model.addAttribute(MCQ_DTO, mcqDto);
+
+		return FO_CREATE_MULTIPLE_ANS_QUESTION;
+	}
+
+	@PostMapping("/multiple-ans")
+	public String saveMultipleAnswerQuestion(Model model, final @ModelAttribute(MCQ_DTO) MCQDto mcqDto,
+			final BindingResult result) {
+		log.info("===== START create multiple answer question form =====");
+		Question question = questionConverter.toEntity(mcqDto);
+
+		questionService.save(question);
+		log.info("{}", question);
+		log.info("===== CREATE multiple answer question form END =====");
+
+		return REDIRECT_FO_USER_QUESTION;
+	}
+
 }
